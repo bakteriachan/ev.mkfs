4180961829925c8c46f2365b5256fa79a73fa0d3
bakteriachan <josuegarcia17904@gmail.com>
2025-08-23 21:56:12 -0300

    ext2: write superblock and superblock backup

--- fs/ext2/ext2.h
+++ fs/ext2/ext2.h
@@ -25,14 +25,16 @@ int write_uint32_le(int fd, uint32_t value, void *buff) {
 	return write(fd, buff, sizeof(uint32_t));
 }
 
-void write_superblock(int fd, struct ext2_superblock *buff) {
-	void *write_buff = malloc(64);
-	off_t file_size = lseek(fd, 0, SEEK_END);
+uint32_t superblock_groups_count(struct ext2_superblock* sb) {
+	return sb->s_blocks_count / sb->s_blocks_per_group;
+}
 
+void write_superblock(int fd, struct ext2_superblock *buff) {
+	off_t file_size = lseek(fd, 0, SEEK_END);
 	const uint32_t blocksize = 4096;
 
 	struct ext2_superblock *sb = malloc(sizeof(struct ext2_superblock));
-	sb->s_blocks_count = file_size / blocksize;
+	sb->s_blocks_count = file_size / (long)blocksize;
 	
 	sb->s_rev_level = EXT2_DYNAMIC_REV;
 	sb->s_magic = EXT2_SUPER_MAGIC;
@@ -40,19 +42,16 @@ void write_superblock(int fd, struct ext2_superblock *buff) {
 	sb->s_log_frag_size = 2;
 	sb->s_first_data_block = blocksize > 1024 ? 0 : 1;
 	
-	sb->s_blocks_per_group = 1024;
-	sb->s_frags_per_group = 1024;
+	sb->s_blocks_per_group = blocksize * 8;
+	sb->s_frags_per_group = sb->s_blocks_per_group;
 
-	uint32_t groups_count = sb->s_blocks_count / sb->s_blocks_per_group;
-	
-	sb->s_inode_size = 1 << sb->s_log_block_size;
-	sb->s_inodes_per_group = blocksize * 8;
-	sb->s_inodes_count = sb->s_inodes_per_group;
+	uint32_t groups_count = superblock_groups_count(sb);
+	sb->s_inode_size = 256;
+	sb->s_inodes_per_group = blocksize * 2;
+	sb->s_inodes_count = sb->s_inodes_per_group * groups_count;
 
-
-	sb->s_r_blocks_count = 1024;
-	sb->s_free_blocks_count = 0;
-	sb->s_free_inodes_count = 0;
+	sb->s_free_blocks_count = sb->s_blocks_count;
+	sb->s_free_inodes_count = sb->s_inodes_count;
 
 	sb->s_max_mnt_count = 25;
 
@@ -77,20 +76,27 @@ void write_superblock(int fd, struct ext2_superblock *buff) {
 
 
 	lseek(fd, 1024, SEEK_SET);
-	write_uint32_le(fd, sb->s_inodes_count, write_buff);
 
-	write_uint32_le(fd, sb->s_blocks_count, write_buff);
+	write(fd, sb, sizeof(struct ext2_superblock));
+	memcpy(buff, sb, sizeof(struct ext2_superblock));
+}
 
-
-	memcpy(buff, sb, sizeof(sb));
-	free(write_buff);
+void backup_superblock(int fd, struct ext2_superblock *sb) {
+	uint32_t groups_count = superblock_groups_count(sb);
+	for(int i = 1; i <= groups_count; i++) {
+		if(i == 1 || i%3 == 0 || i%5 == 0 || i%7 == 0) {
+			sb->s_block_group_nr = i;
+			lseek(fd, sb->s_blocks_per_group * (1024 << sb->s_log_block_size) * i, SEEK_SET);
+			write(fd, sb, sizeof(struct ext2_superblock));
+		}
+	}
 }
 
 void create_ext2_filesystem(int fd) {
 	struct ext2_superblock *superblock = malloc(sizeof(struct ext2_superblock));
 
 	write_superblock(fd, superblock);
-
+	backup_superblock(fd, superblock);
 
 	free(superblock);
 }
